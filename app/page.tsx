761e48ed831505fc185846659714e2d211a619f0
[Adarsh Menon] <[s2088639]@sms.ed.ac.uk>
2024-11-21 18:54:44 +0000

    feat: init commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import "./app.css";
+
+export default function Home() {
+  return (
+    <div className="main-page">
+      <div className="page-title-wrapper">
+        <h1 className="page-title">Pott Farm</h1>
+      </div>
+    </div>
+  );
+}
