b3fd595102ba6f869cb15685d356869cafb0d2cd
[Adarsh Menon] <adarshmenonn@gmail.com>
2024-11-25 18:10:22 +0000

    fix : weird title movement due to link within link

--- app/components/title.tsx
+++ app/components/title.tsx
@@ -6,8 +6,7 @@ import { FC, useState } from "react";
 const Title: FC = () => {
   const [titleEntered, setTitleEntered] = useState(false);
   return (
-    <Link
-      href={"./"}
+    <span
       className="page-title"
       onMouseEnter={() => setTitleEntered(true)}
       onMouseLeave={() => setTitleEntered(false)}
@@ -18,7 +17,7 @@ const Title: FC = () => {
       <label className={titleEntered ? "style-b" : "style-a"}>
         <label className="extra-title">Farm</label>
       </label>
-    </Link>
+    </span>
   );
 };
 
