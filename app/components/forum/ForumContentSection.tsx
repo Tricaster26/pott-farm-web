95244d1b383f8b7e0e954d66308f218ed99ac910
[Adarsh Menon] <adarshmenonn@gmail.com>
2024-12-16 18:44:19 +0000

    fix: added opacity to backgrounds on fourm containers

--- app/components/forum/ForumContentSection.tsx
+++ app/components/forum/ForumContentSection.tsx
@@ -1,17 +1,18 @@
 import { FC } from "react";
 import AnimatedContainer from "../QuoteBox";
+import "./forum-content.scss";
 
 const ForumContentSection: FC = () => {
   return (
     <div className="w-full p-16 flex flex-col gap-[20px]">
       <AnimatedContainer customClass="forum-animated" animateProperty="width">
-        <div className="w-full bg-slate-700 rounded-lg px-5 py-2 pt-3 border-2">
+        <div className="bg-[#334155dd] w-full rounded-lg px-5 py-2 pt-3 border-2">
           <label className="newsreader text-[42px] text-slate-50">
             Welcome to the Forest
           </label>
         </div>
 
-        <div className="w-full h-40 bg-[#484848] rounded-lg px-5 py-2 pt-3 border-2 border-[#000000]">
+        <div className="w-full h-40 bg-[#484848DD] rounded-lg px-5 py-2 pt-3 border-2 border-[#000000]">
           <div></div>
         </div>
       </AnimatedContainer>
