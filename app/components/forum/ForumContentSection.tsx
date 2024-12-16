6ec658c7a91494f4da1fd5f15c551a34c9c6cc02
[Adarsh Menon] <adarshmenonn@gmail.com>
2024-12-16 17:11:14 +0000

    fix : added update

--- app/components/forum/ForumContentSection.tsx
+++ app/components/forum/ForumContentSection.tsx
@@ -2,11 +2,15 @@ import { FC } from "react";
 
 const ForumContentSection: FC = () => {
   return (
-    <div className="w-full p-16">
-      <div className="w-full bg-slate-700 rounded-lg px-5 py-2 border-2">
-        <label className="text-[42px] text-slate-50">
+    <div className="w-full p-16 flex flex-col gap-[20px]">
+      <div className="w-full bg-slate-700 rounded-lg px-5 py-2 pt-3 border-2">
+        <label className="newsreader text-[42px] text-slate-50">
           Welcome to the Forest
         </label>
+      </div>
+
+      <div className="w-full h-40 bg-[#484848] rounded-lg px-5 py-2 pt-3 border-2 border-[#000000]">
+        <div></div>
       </div>
     </div>
   );
